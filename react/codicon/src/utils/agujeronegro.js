6c55c52f1ad4dcf364456c20e8e0ddebce8f3b99
Luciano16-gif <111302468+Luciano16-gif@users.noreply.github.com>
2025-06-03 18:25:55 -0400

    Move agujeronegro utility

--- react/codicon/src/utils/agujeronegro.js
+++ react/codicon/src/utils/agujeronegro.js
@@ -1,12 +1,14 @@
 function Nose() {
     const portal = document.getElementById('portal');
 
-    for (let i = 0 ; i < 360; i++) {
-        const chispa = document.createElement("div");
-        chispa.className = "chispa";
+    for (let i = 0; i < 360; i++) {
+        const chispa = document.createElement('div');
+        chispa.className = 'chispa';
         chispa.style.transform = 'rotate(' + i * 2 + 'deg)';
-        let chispaTranslation = Math.random() * (120 - 100) + 100;
+        const chispaTranslation = Math.random() * (120 - 100) + 100;
         chispa.style.transform += ' translateY(' + chispaTranslation + 'px)';
         portal.appendChild(chispa);
     }
 }
+
+export default Nose;
